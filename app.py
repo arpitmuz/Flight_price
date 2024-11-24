71c6b9de51793c5620be112dc393e77a9f1c1696
Arpit-Agrawal <arpitmfp9@gmail.com>
2024-11-24 11:34:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ st.title("Flight Fare Prediction System:")
 
 # DATE OF JOURNEY
 date_dep=st.date_input("Enter Your Departure Date:",value=None)
-Journey_day = int(pd.to_datetime(date_dep, format="%Y-%m-%dT").day)
-Journey_Month = int(pd.to_datetime(date_dep, format="%Y-%m-%dT").month)
+Journey_day = int(pd.to_datetime(date_dep, format="%YYYY-%MM-%DD").day)
+Journey_Month = int(pd.to_datetime(date_dep, format="%YYYY-%MM-%DD").month)
 # print(Journey_day,Journey_Month)
 #  DEPARTURE TIME
 dep_time=st.time_input("Enter Your Departure Time:")
